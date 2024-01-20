37841ef18f578ffe6f60e7c4f1d5209cc4509687
Wang-hsiu-chen <wang.tw.ted@gmail.com>
2024-01-21 01:58:00 +0800

    fix/TB3_simulation

---
+++ devel/share/turtlebot3_msgs/cmake/turtlebot3_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(turtlebot3_msgs_MESSAGE_FILES "/home/twang/Turtlebot3_AutoRace/src/turtlebot3_msgs/msg/SensorState.msg;/home/twang/Turtlebot3_AutoRace/src/turtlebot3_msgs/msg/VersionInfo.msg;/home/twang/Turtlebot3_AutoRace/src/turtlebot3_msgs/msg/Sound.msg")
+set(turtlebot3_msgs_SERVICE_FILES "")
