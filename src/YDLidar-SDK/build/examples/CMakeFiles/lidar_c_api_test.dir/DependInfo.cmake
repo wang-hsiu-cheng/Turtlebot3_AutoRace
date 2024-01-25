d8879a005f123b24d72ba4578937d28c02171669
wintera1233 <zhen.cc.1207@gmail.com>
2024-01-25 12:10:49 +0800

    modified lidar_driver position, test

---
+++ src/YDLidar-SDK/build/examples/CMakeFiles/lidar_c_api_test.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/miffy1233/racing/Turtlebot3_AutoRace/src/lidar1/YDLidar-SDK/examples/lidar_c_api_test.c" "/home/miffy1233/racing/Turtlebot3_AutoRace/src/lidar1/YDLidar-SDK/build/examples/CMakeFiles/lidar_c_api_test.dir/lidar_c_api_test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../core"
+  "../src"
+  "../"
+  "../.."
+  "examples"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/miffy1233/racing/Turtlebot3_AutoRace/src/lidar1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
