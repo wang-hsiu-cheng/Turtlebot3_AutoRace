7f354fd8e3549f0d62bcd92f8d08a5c7dd76b789
Wang-hsiu-chen <wang.tw.ted@gmail.com>
2024-01-30 01:36:50 +0800

    install nevigation packages

---
+++ devel/share/gennodejs/ros/tf2_msgs/msg/_index.js
@@ -0,0 +1,24 @@
+
+"use strict";
+
+let TF2Error = require('./TF2Error.js');
+let TFMessage = require('./TFMessage.js');
+let LookupTransformAction = require('./LookupTransformAction.js');
+let LookupTransformActionGoal = require('./LookupTransformActionGoal.js');
+let LookupTransformFeedback = require('./LookupTransformFeedback.js');
+let LookupTransformGoal = require('./LookupTransformGoal.js');
+let LookupTransformActionFeedback = require('./LookupTransformActionFeedback.js');
+let LookupTransformActionResult = require('./LookupTransformActionResult.js');
+let LookupTransformResult = require('./LookupTransformResult.js');
+
+module.exports = {
+  TF2Error: TF2Error,
+  TFMessage: TFMessage,
+  LookupTransformAction: LookupTransformAction,
+  LookupTransformActionGoal: LookupTransformActionGoal,
+  LookupTransformFeedback: LookupTransformFeedback,
+  LookupTransformGoal: LookupTransformGoal,
+  LookupTransformActionFeedback: LookupTransformActionFeedback,
+  LookupTransformActionResult: LookupTransformActionResult,
+  LookupTransformResult: LookupTransformResult,
+};
