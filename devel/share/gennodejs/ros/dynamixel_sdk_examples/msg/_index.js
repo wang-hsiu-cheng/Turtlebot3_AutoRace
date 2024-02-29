f5ac30bf298de05e41011e7276c2c16ca2776bdc
wintera1233 <zhen.cc.1207@gmail.com>
2024-03-01 01:05:36 +0800

    add dynamixel_sdk src & catkin

---
+++ devel/share/gennodejs/ros/dynamixel_sdk_examples/msg/_index.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+let BulkSetItem = require('./BulkSetItem.js');
+let SyncSetPosition = require('./SyncSetPosition.js');
+let SetPosition = require('./SetPosition.js');
+
+module.exports = {
+  BulkSetItem: BulkSetItem,
+  SyncSetPosition: SyncSetPosition,
+  SetPosition: SetPosition,
+};
---
+++ devel/share/gennodejs/ros/dynamixel_sdk_examples/srv/_index.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+let SyncGetPosition = require('./SyncGetPosition.js')
+let BulkGetItem = require('./BulkGetItem.js')
+let GetPosition = require('./GetPosition.js')
+
+module.exports = {
+  SyncGetPosition: SyncGetPosition,
+  BulkGetItem: BulkGetItem,
+  GetPosition: GetPosition,
+};
