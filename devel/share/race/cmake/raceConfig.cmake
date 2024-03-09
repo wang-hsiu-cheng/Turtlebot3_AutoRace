af7be71572496929d077b57e2c724d4f29d7c7a2
Wang-hsiu-chen <wang.tw.ted@gmail.com>
2024-03-09 23:54:13 +0800

    again

--- devel/share/race/cmake/raceConfig.cmake
+++ devel/share/race/cmake/raceConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/twang/Turtlebot3_AutoRace/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /home/twang/Turtlebot3_AutoRace/devel/lib;/home/twang/Turtlebot3_AutoRace/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
@@ -185,7 +185,7 @@ foreach(t ${race_EXPORTED_TARGETS})
   endif()
 endforeach()
 
-set(depends "geometry_msgs;roscpp;rospy;std_msgs")
+set(depends "")
 foreach(depend ${depends})
   string(REPLACE " " ";" depend_list ${depend})
   # the package name of the dependency must be kept in a unique variable so that it is not overwritten in recursive calls
