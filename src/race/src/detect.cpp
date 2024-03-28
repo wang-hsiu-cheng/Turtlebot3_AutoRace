ff47c27ee222aa199d460675ac47fada452b43ed
TWang <wang.tw.ted@gmail.com>
2024-03-28 22:37:08 +0800

    structure     detecting loop logic

--- src/race/src/detect.cpp
+++ src/race/src/detect.cpp
@@ -4,17 +4,29 @@ void runAndDetectImage(const int sign_number)
 {
     double x, y, z;
     int testCounter = 0;
+    int loopTime = 20;
+    int loopTiemCounter = 0;
+    int error = 0;
+    int errorThreshold = 20;
+    double slop = 0;
 
     do
     {
-        testCounter ++;
-        CAMERA1::detectRoad();
-        CAMERA1::isDetected = !CAMERA1::isDetected;
-        WHEEL::move_front(3, 0);
-
-        VISION::takingPhoto(sign_number);
+        testCounter++;
+        if (loopTiemCounter == loopTime)
+        {
+            CAMERA1::detectRoad();
+            VISION::takingPhoto(sign_number);
+            if (CAMERA1::isDetected)
+                slop = CAMERA1::getSlop();
+            else
+                error++;
+            loopTiemCounter = 0;
+        }
+        loopTiemCounter++;
+        WHEEL::move_front(3, slop);
+        if (error == errorThreshold)
+            WHEEL::stop();
     } while (!VISION::isDetected && testCounter < 20);
-
-    VISION::isDetected = !VISION::isDetected;
     return;
 }
