494295725c0582211f0d0650bd2f536402b5ec3a
Wang-hsiu-chen <wang.tw.ted@gmail.com>
2024-05-18 18:09:50 +0800

    get start position add start position params

--- src/tb3_navigation/include/tb3_navigation/navigation_send_goal.h
+++ src/tb3_navigation/include/tb3_navigation/navigation_send_goal.h
@@ -2,10 +2,12 @@
 #define _SEND_GOAL_H_
 
 
-
-double x;
-double y;
-double theta;
+double tf_x;
+double tf_y;
+double tf_theta;
+double x = 1.0;
+double y = 0.0;
+double theta = 1.0;
 // bool goalReached = false;
 
 #endif
