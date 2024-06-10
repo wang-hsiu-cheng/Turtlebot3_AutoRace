1d0438324415f8b259aa6de98ac40d754060b07b
wintera1233 <zhen.cc.1207@gmail.com>
2024-06-11 01:31:43 +0800

    modified sutructure of detect files

--- src/race/include/race/detect.h
+++ src/race/include/race/detect.h
@@ -4,11 +4,26 @@
 #include "race/vision.h"
 #include "race/roadCamera.h"
 #include "race/wheel.h"
+#include "ros/ros.h"
+#include <iostream>
+#include "std_msgs_Float32.h"
+#include <math.h>
+
+ros::Subscriber vl53_subscriber; // Topic: mecanum_fromSTM
+std_msgs::Float32 vl53_sub;
 
 // #define stage1_yaml "/home/Turtlebot_AutoRace/src/race/path/stage1.yaml"
+namespace DETECT
+{
+    void init(ros::NodeHandle nh);
+    void callback(const std_msgs::Float32::ConstPtr &msg);
 
-void runAndDetectImage(const int);
-void fanceDetect(void);
-void turnSignDetect(void);
-void positionCheck(void);
+    void runAndDetectImage(const int);
+    int fanceDetect(void);
+    int turnSignDetect(void);
+    void positionCheck(void);
+
+
+}
+
 #endif
--- src/race/src/detect.cpp
+++ src/race/src/detect.cpp
@@ -1,5 +1,21 @@
 #include "race/detect.h"
+#include "ros/ros.h"
+#include <iostream>
+#include "stdmsgs_Float32.h"
+#include "race/wheel.h"
+#include <math.h>
+ros::Subscriber vl53_receiver; // Topic: mecanum_fromSTM
+std_msgs::Float32 vl53_distance;
 
+void DETECT::init(ros::NodeHandle nh)
+{
+    vl53_subscriber = nh.subscribe("/distance",1,DETECT::callback);
+}
+
+void DETECT::callback(const std_msgs::Float32::ConstPtr &msg){
+    vl53_sub.data = msg->data;
+    ROS_INFO("distance: %f",vl53_sub.data);
+}
 void runAndDetectImage(const int sign_number)
 {
     double x, y, z;
@@ -30,13 +46,71 @@ void runAndDetectImage(const int sign_number)
     } while (!VISION::isDetected && testCounter < 20);
     return;
 }
-void turnSignDetect()
+void DETECT::turnSignDetect()
 {
+    ros::Rate loop_rate(10);
+    float a = 10000, b = 10000, c = 10000;
+    a = vl53_sub.data / 1000;
+    loop_rate.sleep();
+    ros::spinOnce();
+    WHEEL::moveTo(0,0.52359877);
+    ros::spinOnce;
+    b = vl53_sub.data / 1000;;
+    loop_rate.sleep();
+    WHEEL::moveTo(0,1.04719753);
+    ros::spinOnce;
+    c = vl53_sub.data / 1000;;
+    loop_rate.sleep();
+    WHEEL::moveTo(0,-0.52359877);
+    if (abs(0.1 - a) <= 0.03)
+    {
+      
+    }
+    else if (abs(0.1 - b) <= 0.03)
+    {
+       
+    }
+    else if (abs(0.1 - c) <= 0.03)
+    {
+     
+    }
+    else if (abs(a-b)>0.1)
+    {
+        if(a<=b)
+        {
+
+        }
+        else{
+
+        }
+        
+    }
+    else
+    {
+        if (a>=c && b>=c)
+
+        else if (a<=c && b>=c)
+
+        else 
+    }
     return;
 }
-void fanceDetect()
+int fanceDetect()
 {
-    return;
+    ros::Rate loop_rate(10);
+    ros::spinOnce();
+    float c = vl53_sub.data / 1000;
+    float prev_c = 10000;
+    if (c <= 0.05 && c >=0.1)
+    {
+        prev_c = c;
+        return(1);
+    }
+        
+    else{
+        if(abs(prev_c - c)>=0.05)
+        return(2);
+    }  
 }
 void positionCheck()
 {
