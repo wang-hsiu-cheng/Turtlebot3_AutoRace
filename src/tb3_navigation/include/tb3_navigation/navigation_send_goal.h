4acf7b620344ab8f96c004700e4ef46038343059
TWang <wang.tw.ted@gmail.com>
2024-06-26 08:32:23 +0800

    navigation main function

--- src/tb3_navigation/include/tb3_navigation/navigation_send_goal.h
+++ src/tb3_navigation/include/tb3_navigation/navigation_send_goal.h
@@ -1,6 +1,8 @@
 #ifndef _SEND_GOAL_H_
 #define _SEND_GOAL_H_
 
+#define goal_yaml "/home/ditrobotics/Turtlebot3_AutoRace/src/tb3_navigation/params/path.yaml"
+
 double tf_x;
 double tf_y;
 double tf_theta;
@@ -9,5 +11,7 @@ double x = 1.0;
 double y = 0.0;
 double theta = 1.0;
 // bool goalReached = false;
+
+int SendGoal(double x, double y);
 
 #endif
--- src/tb3_navigation/src/navigation_send_goal.cpp
+++ src/tb3_navigation/src/navigation_send_goal.cpp
@@ -8,23 +8,25 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+void Init()
+{
+    nh_send_goal.getParam("navigation_send_goal/goal_x", x);
+    nh_send_goal.getParam("navigation_send_goal/goal_y", y);
+    nh_send_goal.getParam("navigation_send_goal/goal_theta", theta);
+}
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "navigation_send_goal");
     ros::NodeHandle nh_send_goal;
     tf2_ros::Buffer tfBuffer;
     tf2_ros::TransformListener tfListener(tfBuffer);
-    ROS_INFO("goal node running\n");
-
-    nh_send_goal.getParam("navigation_send_goal/goal_x", x);
-    nh_send_goal.getParam("navigation_send_goal/goal_y", y);
-    nh_send_goal.getParam("navigation_send_goal/goal_theta", theta);
-
-    MoveBaseClient ac("move_base", true);
-    while (!ac.waitForServer(ros::Duration(5.0)))
-    {
-        ROS_INFO("waiting for move_bae server come up");
-    }
+    // ROS_INFO("goal node running\n");
+    // MoveBaseClient ac("move_base", true);
+    // while (!ac.waitForServer(ros::Duration(5.0)))
+    // {
+    //     ROS_INFO("waiting for move_base server come up");
+    // }
+    Init();
 
     ROS_INFO("Get current position\n");
     geometry_msgs::TransformStamped transformStamped;
@@ -32,39 +34,69 @@ int main(int argc, char **argv)
     tf_x = transformStamped.transform.translation.x;
     tf_y = transformStamped.transform.translation.y;
     tf_theta = transformStamped.transform.rotation.w;
-    // tf_thetaAngle = tf2::createQuaternionMsgFromYaw(double angle);
     printf("tf_x: %g, tf_y: %g tf_theta: %g\n", tf_x, tf_y);
-    ROS_INFO("Start send goal\n");
-    move_base_msgs::MoveBaseGoal goal;
+
+    Node pathConfig = LoadFile(goal_yaml);
+    for (auto point : pathConfig)
+    {
+        x = point["da"][0].as<double>();
+        y = point["da"][1].as<double>();
+        SendGoal(x, y);
+    }
+    // ROS_INFO("Start send goal\n");
+    // move_base_msgs::MoveBaseGoal goal;
+    // goal.target_pose.header.frame_id = "map";
+    // goal.target_pose.header.stamp = ros::Time::now();
+    // goal.target_pose.pose.position.x = tf_x + x;
+    // goal.target_pose.pose.position.y = tf_y + y;
+    // goal.target_pose.pose.orientation.w = tf_theta + theta;
+    // goal.target_pose.pose.orientation.x = transformStamped.transform.rotation.x;
+    // goal.target_pose.pose.orientation.y = transformStamped.transform.rotation.y;
+    // goal.target_pose.pose.orientation.z = transformStamped.transform.rotation.z;
+    // ac.sendGoal(goal);
+    // ROS_INFO("send Nav end\n");
+
+    // ac.waitForResult();
+    // if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    // {
+    //     printf("yes!");
+    //     // std::string command = "^C ";
+    //     // int result = system(command.c_str());
+    //     return 1;
+    // }
+    // else
+    // {
+    //     ROS_INFO("no");
+    //     return 0;
+    // }
+}
+int SendGoal(double _x, double _y)
+{
+    MoveBaseClient ac("move_base", true);
+    while (!ac.waitForServer(ros::Duration(5.0)))
+    {
+        ROS_INFO("waiting for move_base server come up");
+    }
+    x = _x;
+    y = _y;
+
     goal.target_pose.header.frame_id = "map";
     goal.target_pose.header.stamp = ros::Time::now();
     goal.target_pose.pose.position.x = tf_x + x;
     goal.target_pose.pose.position.y = tf_y + y;
     goal.target_pose.pose.orientation.w = tf_theta + theta;
-    // goal.target_pose.pose.orientation.x = transformStamped.transform.rotation.x;
-    // goal.target_pose.pose.orientation.y = transformStamped.transform.rotation.y;
-    // goal.target_pose.pose.orientation.z = transformStamped.transform.rotation.z;
+
     ac.sendGoal(goal);
-    ROS_INFO("send Nav end\n");
 
     ac.waitForResult();
     if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
     {
         printf("yes!");
-        // std::string command = "^C ";
-        // int result = system(command.c_str());
         return 1;
-        // goalReached = true;
     }
     else
     {
-        ROS_INFO("no");
+        printf("no!");
         return 0;
-        // goalReached = false;
     }
-
-    // if (goalReached)
-    //     return 0;
-    // else if (!goalReached)
-    //     return 1;
 }
